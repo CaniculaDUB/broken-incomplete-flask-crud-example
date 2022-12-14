674b8cffc8cc442fb663e0e4a9a4405f6a96f6ce
CaniculaDUB <114599649+CaniculaDUB@users.noreply.github.com>
2022-12-14 13:01:44 +0100

    Update appp.py

--- appp.py
+++ appp.py
@@ -30,6 +30,7 @@ def delete():
   name = request.args.get('name')
   cur = mysql.connection.cursor()
   s=f"DELETE FROM students where studentName='{name}'"
+  print(s)
   cursor.execute(s)
   mysql.connection.commit()
   
